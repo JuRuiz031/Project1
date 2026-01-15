69a179dbe7a910bb20230651c225e8ee4fce0dfd
seanDtamu <sean21tamu@tamu.edu>
2026-01-15 08:18:34 -0600

    set up routing for account-view

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AccountView } from './account-view/account-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'account', component: AccountView },
+    // { path: '', redirectTo: 'account', pathMatch: 'full' },
+];
