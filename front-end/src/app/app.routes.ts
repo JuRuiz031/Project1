31e06f40f127982d7ff8476c360beb6be01a6fa0
JFRuiz <77316037+JuRuiz031@users.noreply.github.com>
2026-01-14 19:00:08 -0600

    added create-account page

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { CreateAccount } from './create-account/create-account';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'create-account', component: CreateAccount },
   { path : '', redirectTo: '/login', pathMatch: 'full' }
 ];
