5797aad292e54abe4a35978ea3b610efd82f6101
Juan Ruiz <77316037+JuRuiz031@users.noreply.github.com>
2026-01-20 14:07:53 -0600

    Began jwt implementation

---
+++ back-end/src/main/java/com/example/calendario/util/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.example.calendario.util;
+
+// Java imports
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+
+// Spring Framework imports
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+// JWT library imports
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+// Cryptography imports
+import javax.crypto.SecretKey;
+
+public class JwtUtil {
+    
+}
