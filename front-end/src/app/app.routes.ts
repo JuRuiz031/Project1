d20e1b010eb4930868aeea0863e417d214aeb318
Benjamin Tuley <xmonarcx@yahoo.com>
2026-01-21 22:36:20 -0600

    fix file path in app.routes.ts

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,24 +1,24 @@
 import { Routes } from '@angular/router';
-import { AccountView } from './features/account-view/account-view';
+import { AccountView } from './features/auth/account-view/account-view';
 import { Login } from './features/auth/login/login';
 import { CreateAccount } from './features/auth/create-account/create-account';
-import { CreateCalendar } from './features/create-calendar/create-calendar';
+import { CreateCalendar } from './features/calendar/create-calendar/create-calendar';
 
 import { MainPageComponent } from './features/dashboard/main-page/main-page';
 
-import { EditUser } from './features/edit-user/edit-user';
-import { ViewCalendarGroup } from './features/view-calendar-group/view-calendar-group';
-import { CreateEvent } from './features/create-event/create-event';
-import { CreatePoll } from './features/create-poll/create-poll';
-import { CreateCalendarGroup } from './features/create-calendar-group/create-calendar-group';
-import { EditCalendarGroup } from './features/edit-calendar-group/edit-calendar-group';
-import { DeleteCalendarGroup } from './features/delete-calendar-group/delete-calendar-group';
-import { ViewEvent } from './features/view-event/view-event';
-import { EditEvent } from './features/edit-event/edit-event';
-import { DeleteEvent } from './features/delete-event/delete-event';
-import { ViewPoll } from './features/view-poll/view-poll';
-import { EditPoll } from './features/edit-poll/edit-poll';
-import { DeletePoll } from './features/delete-poll/delete-poll';
+import { EditUser } from './features/auth/edit-user/edit-user';
+import { ViewCalendarGroup } from './features/calendar/view-calendar-group/view-calendar-group';
+import { CreateEvent } from './features/event/create-event/create-event';
+import { CreatePoll } from './features/poll/create-poll/create-poll';
+import { CreateCalendarGroup } from './features/calendar/create-calendar-group/create-calendar-group';
+import { EditCalendarGroup } from './features/calendar/edit-calendar-group/edit-calendar-group';
+import { DeleteCalendarGroup } from './features/calendar/delete-calendar-group/delete-calendar-group';
+import { ViewEvent } from './features/event/view-event/view-event';
+import { EditEvent } from './features/event/edit-event/edit-event';
+import { DeleteEvent } from './features/event/delete-event/delete-event';
+import { ViewPoll } from './features/poll/view-poll/view-poll';
+import { EditPoll } from './features/poll/edit-poll/edit-poll';
+import { DeletePoll } from './features/poll/delete-poll/delete-poll';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
