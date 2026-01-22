7380195abdcb7cb21b1fae4d3e1f325aba46881e
Benjamin Tuley <xmonarcx@yahoo.com>
2026-01-21 22:33:58 -0600

    restructure and routing
    grouped pages into like purpose. routed the 'Create New Calendar' button

--- front-end/src/app/features/dashboard/main-page/components/calendar-options/calendar-options.ts
+++ front-end/src/app/features/dashboard/main-page/components/calendar-options/calendar-options.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-calendar-options',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './calendar-options.html',
   styleUrl: './calendar-options.css',
 })
